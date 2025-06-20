a36d676f9a31475425e90567f80db7ea7fff5edf
Kyler Cressall <kyler@cressall.net>
2025-06-20 16:15:56 -0600

    Added the network map and typing changes

--- apps/backend/src/lib/supabase/server.ts
+++ apps/backend/src/lib/supabase/server.ts
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!; // server-only key
+let supabaseUrl = process.env.SUPABASE_URL!;
+let supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!; // server-only key
+
+supabaseUrl = "https://rdeiyagvoovjkiopydaa.supabase.co";
+supabaseServiceKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkZWl5YWd2b292amtpb3B5ZGFhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0ODkyNDgxMSwiZXhwIjoyMDY0NTAwODExfQ.dcKM6PSpAAwvuHdCqhCeDq5guyZMj96RMEJBzYH1XTs";
 
 // export const supabase = createClient(supabaseUrl, supabaseServiceKey);
 export const supabase = createClient(supabaseUrl, supabaseServiceKey);
